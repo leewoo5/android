63aa8dae7fb40eeb7bba0e2e4c8d79dd3a7c95a4
unknown <leewoo5629@gmail.com>
2017-07-07 00:12:31 +0900

    second commit

--- MyApplication/app/src/main/java/com/topfactory/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/topfactory/myapplication/MainActivity.java
@@ -2,12 +2,34 @@ package com.topfactory.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private EditText mEditText;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButton = (Button) findViewById(R.id.button);
+
+        View.OnClickListener firstClickListner = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = mEditText.getText().toString();
+                mTextView.setText(text);
+                mEditText.setText("");
+            }
+        };
+        mButton.setOnClickListener(firstClickListner);
+
     }
 }
